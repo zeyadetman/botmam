801556154b903552a17ab03421028b160a49524a
Zeyad Etman <zeyadetman@gmail.com>
2020-05-08 05:55:19 +0200

    Updates

---
+++ precache-manifest.817078ae7caee8f63fdc7fce813b98aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42d752ac55ff6ba112390ccd72ca051",
+    "url": "/botmam/index.html"
+  },
+  {
+    "revision": "9124c45f7965f81ac30c",
+    "url": "/botmam/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "7b391a4945a64262286b",
+    "url": "/botmam/static/css/main.1f1c6a66.chunk.css"
+  },
+  {
+    "revision": "9124c45f7965f81ac30c",
+    "url": "/botmam/static/js/2.34530cb7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/botmam/static/js/2.34530cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b391a4945a64262286b",
+    "url": "/botmam/static/js/main.9bc50e83.chunk.js"
+  },
+  {
+    "revision": "252364c89c080dfc61db",
+    "url": "/botmam/static/js/runtime-main.1c703275.js"
+  }
+]);
